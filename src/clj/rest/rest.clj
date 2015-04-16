ea69b7ffb72f671d6244b6b6b8c3db4920cd30da
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-16 19:35:52 +0200

    nothing

--- src/clj/rest/rest.clj
+++ src/clj/rest/rest.clj
@@ -3,4 +3,5 @@
             [clj-http.client :as client]
             [clojure.core.typed :refer [ann Map Any] :as t]))
 
+;;http://finance.yahoo.com/webservice/v1/symbols/COALINDIA.NS/quote?format=json
 (def resp (json/read-str (client/get "http://google.com")))
