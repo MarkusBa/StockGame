72c15dc4d11e34f67af5d6fb65de99b7b66d36fb
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-18 16:37:10 +0200

    Added clojurescript: searching for the right version :(

--- project.clj
+++ project.clj
@@ -5,22 +5,34 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
                  [org.clojure/core.typed "0.2.87"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [korma "0.4.0"]
                  [org.clojure/java.jdbc "0.3.5"] ;; workaround for korma
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [com.theoryinpractise.frege/frege "3.21.586-g026e8d7"]
                  [ch.qos.logback/logback-classic "1.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [om "0.5.3"]
+                 ;[reagent "0.5.0"]
                  [clj-http "1.1.0"]
                  [compojure "1.3.3"]
                  [org.clojure/data.json "0.2.6"]
                  [ring "1.3.2"]]
-  :plugins [[lein-fregec "0.1.0-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.0.5"]
             [lein-typed "0.3.5"]
             [lein-deps-tree "0.1.2"]]
-  :frege-source-paths ["src/frege"]
   :java-source-paths ["src/java"]
   :resource-paths ["resources"]
-  :core.typed {:check [config.parse]}
-  :source-paths ["src/clj"]
-  )
+  ;;:core.typed {:check [config.parse]}
+  :source-paths ["src/clj" "src/cljs"]
+  :cljsbuild
+      {:builds [{
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                          :output-to "resources/public/js/main.js"
+                          :optimizations :simple }}]})
+
+
+
+
