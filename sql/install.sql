8e392e779f728639973143dbf5810285513a6eb3
Markus Barthlen <Markus.Barthlen@doesnotexist.com>
2015-04-25 15:02:46 +0200

    Ordering

--- sql/install.sql
+++ sql/install.sql
@@ -2,24 +2,24 @@
 --psql -d stock
 
 CREATE TABLE player (
-  id integer primary key,
+  id SERIAL primary key,
   name varchar(255)  NOT NULL,
   email varchar(255)  NOT NULL
 );
 
 CREATE TABLE item (
-  id integer primary key,
+  id SERIAL primary key,
   symbol varchar(255)  NOT NULL,
   amount integer,
   price integer,
-  idplayer integer references player (id) ON DELETE CASCADE,
+  idplayer SERIAL references player (id) ON DELETE CASCADE,
   ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX ts_idx ON item (ts);
 
 -- Inserts
-insert into player values (1, 'test', 'test@test.com');
-insert into item values (1, 'CASH', 10000, 1, 1, now());
-insert into item values (2, 'PAH3.DE', 0, 0, 1, now());
+insert into player (name, email) values ('test', 'test@test.com');
+insert into item (symbol, amount, price, idplayer, ts) values ('CASH', 10000, 1, 1, now());
+insert into item (symbol, amount, price, idplayer, ts) values ('PAH3.DE', 0, 0, 1, now());
 
--- src/clj/database/connect.clj
+++ src/clj/database/connect.clj
@@ -1,15 +1,45 @@
 (ns database.connect
   (:require [config.parse :as cf]
             [clojure.tools.logging :as log]
-            [korma.core :as k]
-            [korma.db :refer [defdb postgres]]))
+            [korma.core :as k :refer [defentity select where values insert update fields set-fields]]
+            [korma.db :refer [defdb postgres transaction]])
+  (:import (java.sql Timestamp)))
 
 (def db-spec (cf/load-config "resources/config.clj"))
 
 (defdb db (postgres db-spec))
 
-(k/defentity item)
+(defentity item)
 
 (defn get-items [idplayer]
-  (k/select item
-    (k/where {:idplayer (Integer/parseInt idplayer)})))
+  (select item
+    (where {:idplayer (Integer/parseInt idplayer)})))
+
+;; (db/order "YHOO" 2 44.52 "1")
+(defn order [ordersymbol amount price idplayer]
+  (transaction
+    (let [money (:amount (first (select item
+                  (fields :amount)
+                  (where {:idplayer (Integer/parseInt idplayer)
+                          :symbol   "CASH"}))))
+          costs (* amount price)]
+      (log/info money)
+      (when (and (not (nil? money)) (>= money costs) )
+        (update item
+          (set-fields {:amount (- money costs)})
+          (where {:idplayer (Integer/parseInt idplayer)
+                          :symbol   "CASH"}))
+        (if-let [existingamount (:amount (first (select item
+                  (fields :amount)
+                  (where {:idplayer (Integer/parseInt idplayer)
+                          :symbol   ordersymbol}))))]
+          (update item
+            (set-fields {:amount (+ existingamount amount)})
+            (where {:idplayer (Integer/parseInt idplayer)
+                          :symbol   ordersymbol}))
+          (insert item
+            (values [{:symbol ordersymbol
+                      :amount amount
+                      :price price
+                      :idplayer (Integer/parseInt idplayer)
+                      :ts (java.sql.Timestamp. (System/currentTimeMillis))}])))))))
